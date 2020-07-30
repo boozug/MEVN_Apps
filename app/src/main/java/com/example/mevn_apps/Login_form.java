cd17ad0f4003fc698b1191de30dd7aa654c2e306
viettiep0412 <viettiep0412@gmail.com>
2020-07-30 15:21:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mevn_apps/Login_form.java
@@ -0,0 +1,59 @@
+ package com.example.mevn_apps;
+
+ import android.content.Intent;
+ import android.os.Bundle;
+ import android.text.method.LinkMovementMethod;
+ import android.view.View;
+ import android.widget.EditText;
+ import android.widget.RelativeLayout;
+ import android.widget.TextView;
+ import android.widget.Toast;
+
+ import androidx.appcompat.app.AppCompatActivity;
+
+
+ public class Login_form extends AppCompatActivity {
+     // declare
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_login_form);
+         RelativeLayout btnLogin = (RelativeLayout) findViewById(R.id.relative_layout_btn);
+         final EditText txtUserName = (EditText) findViewById(R.id.TxtUsername);
+         final EditText txtPassword = (EditText) findViewById(R.id.TxtPassword);
+         btnLogin.setOnClickListener(new View.OnClickListener() {
+
+             public void onClick(View view) {
+                 String username = txtUserName.getText().toString();
+                 String password = txtPassword.getText().toString();
+                 try {
+                     if (username.length() > 0 && password.length() > 0) {
+                         DBUserAdapter dbUser = new DBUserAdapter(Login_form.this);
+                         dbUser.open();
+                         if(dbUser.Login(username,password))
+                         {
+                             Toast.makeText(Login_form.this,"Logged In Successfully", Toast.LENGTH_LONG).show();
+                             Intent intent = new Intent(Login_form.this, Devices_list.class);
+                             startActivity(intent);
+                         }
+                         else {
+                             Toast.makeText(Login_form.this,"Invalid Username/Password", Toast.LENGTH_LONG).show();
+                         }
+                         dbUser.close();
+                     }
+                 } catch (Exception e) {
+                     Toast.makeText(Login_form.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                 }
+             }
+         });
+         TextView register = (TextView) findViewById(R.id.textView_register);
+         register.setMovementMethod(LinkMovementMethod.getInstance());
+         register.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(Login_form.this, Signup_Form.class);
+                 startActivity(intent);
+             }
+         });
+     }
+}
---
+++ app/src/main/java/com/example/mevn_apps/Signup_Form.java
@@ -0,0 +1,82 @@
+ package com.example.mevn_apps;
+
+ import android.content.Intent;
+ import android.database.sqlite.SQLiteException;
+ import android.os.Bundle;
+ import android.text.method.LinkMovementMethod;
+ import android.view.View;
+ import android.widget.EditText;
+ import android.widget.RelativeLayout;
+ import android.widget.TextView;
+ import android.widget.Toast;
+
+ import androidx.appcompat.app.AppCompatActivity;
+
+
+ public class Signup_Form extends AppCompatActivity {
+     //region ------------------------------------------------------Main action
+     public static DBUserAdapter.DatabaseHelper database;
+     public static final String DATABASE_NAME = "Accounts";
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_signup__form);
+
+         RelativeLayout btnRegister = (RelativeLayout) findViewById(R.id.relative_layout_btn);
+         final EditText txtUserName = (EditText) findViewById(R.id.TxtUsername);
+         final EditText txtPassword = (EditText) findViewById(R.id.TxtPassword);
+         final EditText txtPasswordrpt = (EditText) findViewById(R.id.TxtRPTPassword);
+         btnRegister.setOnClickListener(new View.OnClickListener() {
+//             @Override
+             public void onClick(View view) {
+                 String username = txtUserName.getText().toString();
+                 String password = txtPassword.getText().toString();
+                 String rptpassword = txtPasswordrpt.getText().toString();
+                 try {
+                     if (username.length() >= 8) {
+                         if (password.equals(rptpassword) ) {
+                             DBUserAdapter dbUser = new DBUserAdapter(Signup_Form.this);
+                             dbUser.open();
+                             try {
+                                 dbUser.AddUser(username,password);
+                                 Toast.makeText(Signup_Form.this,"Finished. Pls check your account in the login form", Toast.LENGTH_LONG).show();
+                                 dbUser.close();
+                             }
+                             catch (SQLiteException e){
+                                 Toast.makeText(Signup_Form.this,e.getMessage(), Toast.LENGTH_LONG).show();
+                             }
+                         }
+                         else {
+                             Toast.makeText(Signup_Form.this,"Pls correct password", Toast.LENGTH_LONG).show();
+                         }
+                     }
+                     else
+                     {
+                         Toast.makeText(Signup_Form.this,"Pls add an correct User", Toast.LENGTH_LONG).show();
+                     }
+
+                 }
+                 catch(Exception e){
+                     Toast.makeText(Signup_Form.this,e.getMessage(), Toast.LENGTH_LONG).show();
+
+                 }
+             }
+         });
+         //endregion
+
+         //region change to login page
+         TextView login = (TextView) findViewById(R.id.textView_login);
+         login.setMovementMethod(LinkMovementMethod.getInstance());
+         login.setOnClickListener(new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(Signup_Form.this, Login_form.class);
+                 startActivity(intent);
+
+             }
+         });
+         // endregion
+     }
+ }
